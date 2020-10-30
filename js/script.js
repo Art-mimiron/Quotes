49f2b6fb10059a88a10f249d5df2a189d161740e
Artem <artmimiron@gmail.com>
2020-10-30 18:51:53 +0200

    +adapt

--- js/script.js
+++ js/script.js
@@ -7,22 +7,35 @@ document.addEventListener('DOMContentLoaded', () => {
         twitter = document.getElementById('twitter'),
         quoteWrapper = document.getElementById('wrapper');
 
+    function generatingQuote() {
+        fetch('https://stormy-waters-81463.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+            .then(request => request.json())
+            .then(data => {
+                quote.innerHTML = data.quoteText;
+                if(data.quoteAuthor) {
+                    quoteAuthor.innerHTML = data.quoteAuthor;
+                } else {quoteAuthor.innerHTML = 'Someone';}
+            })
+            .catch(() => {
+                generatingQuote();
+            });
+    }
+
+    generatingQuote();
+
     generateQuote.addEventListener('click', (e) => {
         generateQuote.classList.remove('active');
         generateQuote.classList.add('inactive');
+        generateQuote.disabled = true;
+        twitter.classList.remove('active');
+        twitter.classList.add('inactive');
+        twitter.disabled = true;
         
         quoteWrapper.classList.add('wrapper_download');
         quote.classList.add('font_hide');
         quoteAuthor.classList.add('font_hide');
 
-        fetch('https://stormy-waters-81463.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
-            .then(request => request.text())
-            .then(request => JSON.parse(request))
-            .then(request => {
-                quote.innerHTML = request.quoteText;
-                quoteAuthor.innerHTML = request.quoteAuthor;
-                console.log(request);
-            });
+        generatingQuote();
         
         setTimeout(()=>{
             quoteWrapper.classList.add('wrapper_done');
@@ -40,14 +53,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 4950);
 
         setTimeout(()=> {
+            generateQuote.disabled = false;
             generateQuote.classList.add('active');
             generateQuote.classList.remove('inactive');
+            twitter.disabled = false;
+            twitter.classList.add('active');
+            twitter.classList.remove('inactive');
         },5600);
         
     });
 
-   /*  generateQuote.addEventListener('click', function(e) {
-        console.log('hi');
-    }); */
+   twitter.addEventListener('click', ()=> {
+        window.open(`https://twitter.com/intent/tweet?text="${quote.innerText}" ${quoteAuthor.innerText}`);
+   });
 
 });
